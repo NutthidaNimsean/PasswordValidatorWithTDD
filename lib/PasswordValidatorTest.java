db105345847c8364e871e15ac17b349234bb4057
NutthidaNimsean <nutthida.nim@ku.th>
2025-07-04 16:42:20 +0700

    Add files via upload

--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -6,12 +6,41 @@ public class PasswordValidatorTest{
         System.out.println("--- Running Password Validator Tests ---");
         
         // Test Case 1: รหัสผ่านสั้นควรจะ INVALID
-        PasswordStrength result1 = PasswordValidator.validate("123");
+        String pw = "123";
+        PasswordStrength result1 = PasswordValidator.validate(pw);
         if (result1 == PasswordStrength.INVALID) {
-            System.out.println("Test Case 1 Passed: Short password is INVALID.");
+            System.out.println("Test Case 1 : "+pw+" : Passed: Short password is INVALID.");
         } else {
-            System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
+            System.out.println("Test Case 1 : "+pw+" : FAILED: Expected INVALID but got " + result1);
         }
+
+        // Test Case 2: รหัสผ่านทั้งหมดเป็นตัวเล็กควรจะ WEAK 
+        pw = "abcdefghij";
+        PasswordStrength result2 = PasswordValidator.validate(pw);;
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 : "+pw+" : Passed: All lowercase password is WEAK.");
+        } else {
+            System.out.println("Test Case 2  : "+pw+" : FAILED: Expected WEAK but got " + result2);
+        }
+
+        // Test Case 3: รหัสผ่านทั้งหมดเป็นตัวเล็กตัวใหญ่ควรจะ MEDIUM 
+        pw = "abcDEFGHIJ";
+        PasswordStrength result3 = PasswordValidator.validate(pw);;
+        if (result3 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 3 : "+pw+" : Passed: All lowercase and uppercase password are MEDIUM.");
+        } else {
+            System.out.println("Test Case 3  : "+pw+" : FAILED: Expected MEDIUM but got " + result3);
+        }
+
+        // Test Case 4: รหัสผ่านทั้งหมดเป็นตัวเล็กตัวใหญ่ตัวเลขควรจะ STRONG 
+        pw = "abcGFG1234";
+        PasswordStrength result4 = PasswordValidator.validate(pw);;
+        if (result4 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 4 : "+pw+" : Passed: All lowercase , uppercase and Number password are STRONG.");
+        } else {
+            System.out.println("Test Case 4  : "+pw+" : FAILED: Expected MEDIUM but got " + result4);
+        }
+
 
         // --- เขียน Test Case อื่นๆ ต่อ ---
 
